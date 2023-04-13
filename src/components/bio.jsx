df451935baf130bb1b6bf5c33097942ddcafd9af
cjtakhar <cjtakhar@gmail.com>
2023-04-13 15:53:00 -0700

    design

--- src/components/bio.jsx
+++ src/components/bio.jsx
@@ -1,17 +1,17 @@
 import Image from './images/kt.jpg'
 
 export default function Bio() {
-    return(
+    return (
         <div>
             <div className="main-container">
                 <div className="title-container">
                     <h1 className="title">Kuljit Kaur Takhar</h1>
                 </div>
                 <div className="bio-container">
-                <img className="bio-img" src={Image} alt="kuljit takhar" />
-                    <div clasName="bio-text">
-                        <p className="text">Kuljit Kaur Takhar is a highly passionate technologist and committed humanitarian based in San Francisco who is dedicated to driving positive change in the world. She has contributed her efforts and experience to the Harvard Humanitarian Initiative by analyzing complex data sets to identify root causes of refugee crisis and recommending corrective actions. These recommendations have been instrumental in guiding governments and industries towards a more effective response to critical issues and predicting future occurrence.</p> 
-                        <p clasName="text">Today, Kuljit applies technology to make impactful, ethical, meaningful change in the way that we live, and as an engineer, works exclusively with mission driven organizations that are committed to making the world a better place. </p>
+                    <img className="bio-img" src={Image} alt="kuljit takhar" />
+                    <div className="bio-text">
+                        <p className="text">Kuljit Kaur Takhar is a highly passionate technologist and committed humanitarian based in San Francisco who is dedicated to driving positive change in the world. She has contributed her efforts and experience to the Harvard Humanitarian Initiative by analyzing complex data sets to identify root causes of refugee crisis and recommending corrective actions. These recommendations have been instrumental in guiding governments and industries towards a more effective response to critical issues and predicting future occurrence.</p>
+                        <p className="text">Today, Kuljit applies technology to make impactful, ethical, meaningful change in the way that we live and, as an engineer, works exclusively with mission driven organizations that are committed to making the world a better place. </p>
                         <p className="text">She believes no human is illegal and trans rights are human rights.</p>
                     </div>
                 </div>
